f6053b95730e0ee66d4f3e174877f2cee2d1463a
martingutpar <m.gutierrezp.2022@alumnos.urjc.es>
2025-02-13 10:22:02 +0100

    Proyecto

---
+++ demo-vscode/src/test/java/es/grupo12/demo_vscode/DemoVscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.grupo12.demo_vscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
