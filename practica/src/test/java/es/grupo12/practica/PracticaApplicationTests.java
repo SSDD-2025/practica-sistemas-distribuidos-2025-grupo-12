820b1876f15c4a19b8e907e1400a4b135169757c
danielmunmar <d.munozm.2022@alumnos.urjc.es>
2025-02-19 13:08:33 +0100

    Creación proyecto

---
+++ practica/src/test/java/es/grupo12/practica/PracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.grupo12.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
