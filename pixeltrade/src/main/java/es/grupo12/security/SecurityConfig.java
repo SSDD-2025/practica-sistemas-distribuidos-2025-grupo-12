4c4f60a635b2f77230ccf539a818f9703b1fbe61
danielmunmar <d.munozm.2022@alumnos.urjc.es>
2025-04-14 17:59:17 +0200

    Solve problems in SecurityConfig

--- pixeltrade/src/main/java/es/grupo12/security/SecurityConfig.java
+++ pixeltrade/src/main/java/es/grupo12/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package es.grupo12.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,11 +11,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -28,7 +24,6 @@ import es.grupo12.security.jwt.UnauthorizedHandlerJwt;
 public class SecurityConfig {
 
 	@Autowired
-	@Lazy
 	private JwtRequestFilter jwtRequestFilter;
 	
 	@Autowired
@@ -57,20 +52,7 @@ public class SecurityConfig {
 		return authProvider;
 	}
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.builder()
-                    .username("user")
-                    .password(passwordEncoder().encode("pass"))
-                    .roles("USER")
-                    .build();
-		UserDetails admin = User.builder()
-					.username("admin")
-					.password(passwordEncoder().encode("adminpass"))
-					.roles("USER","ADMIN")
-					.build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+    
 
 	@Bean
 	@Order(1)
