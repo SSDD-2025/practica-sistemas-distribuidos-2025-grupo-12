f800a7db0749fdf8f341c1668532106f262236f2
danielmunmar <d.munozm.2022@alumnos.urjc.es>
2025-04-23 09:50:04 +0200

    Solve ErrorWebController

--- pixeltrade/src/main/java/es/grupo12/controller/web/ErrorWebController.java
+++ pixeltrade/src/main/java/es/grupo12/controller/web/ErrorWebController.java
@@ -1,12 +1,11 @@
 package es.grupo12.controller.web;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class ErrorWebController implements ErrorController {
+@ControllerAdvice
+public class ErrorWebController {
 
     @RequestMapping("/error")
     public String invalidUrl(Model model) {
